dfa6439973610d1cef20fea45b0814b738c482d8
acccidiccc-tutanota <74606036+acccidiccc-tutanota@users.noreply.github.com>
2020-11-17 14:46:53 +0100

    Create shell.cpp

---
+++ shell.cpp
@@ -0,0 +1,79 @@
+# mindstorm-shell
+#include <iostream>
+#include <string>
+
+
+#define PM1 = 1
+#define PM2 = 2
+#define PM3 = 3
+#define PM4 = 4
+void help();
+void hebelsteuerung(int speed, int turns);
+
+void shell() {
+	//leistung 20 15 centimeter
+	std::cout << "shell # ";
+
+	while (true) {
+		std::string input;
+		std::getline(std::cin, input);
+		if (input == "reichweite") {
+			hebelsteuerung(0, 0);
+		}
+		
+	}
+}
+
+void hebelsteuerung(int speed, int turns) {
+	int choice;
+	int i;
+	go:
+	std::cout << "meter <1> oder umdrehungen<2>\n";
+	std::cin >> i;
+	switch (i)  {
+	    case 1:
+	    std::cout << "umdrehungen, für beenden eine negative zahle eingeben:\n";
+	    
+	    
+    	std::cin >> turns;
+    	if (turns < 0) {
+    		shell();
+    	}
+    	
+	    std::cout << 14 * turns << " cm\n";
+	    shell();
+	    break;
+	    case 2:
+	    float meter;
+	    std::cout << "wie viele meter willst du erreichen:\n";
+	    std::cin >> meter;
+	    std::cout << meter / 14 << "umdrehungen\n";
+	    break;
+	    default:
+	    std::cout << "invalide eingabe\n";
+	    goto go;
+	    
+	}
+	
+}
+void help() {
+
+	std::cout << "reichweite\nhelp\n";
+}
+
+int main() {
+	
+	shell();
+
+
+
+
+
+
+	return 0;
+
+
+
+}
+
+
